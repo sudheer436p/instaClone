cf9fb6b9d51f0b46d1989987734a9fa9cbd5faf4
Sudheer P <sudheerp@Sudheers-iMac.local>
2020-11-22 16:08:17 +0000

    initial commit

---
+++ App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <Text>Entry Point ...</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
